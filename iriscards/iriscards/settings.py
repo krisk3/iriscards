61f5bebd85cfb58ded01194a811767860ae36710
Kuriakose Kuruvila Anathanam <kris.kuruvilla@gmail.com>
2023-05-11 18:01:21 +0530

    Database changed to Postgres and .env file added.

--- iriscards/iriscards/settings.py
+++ iriscards/iriscards/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,11 +86,14 @@ WSGI_APPLICATION = 'iriscards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
